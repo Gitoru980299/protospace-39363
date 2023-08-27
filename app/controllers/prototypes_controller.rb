90c5093e21507f4e0681f08f30e1a70475e8560f
Gitoru980299 <tmatsumoto299@gmail.com>
2023-08-27 23:24:10 +0900

    third commit
    Before action 修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:update]
   
   
   def index
